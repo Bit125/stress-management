4d86e58c25642215e67a9abb14579ba29234c5b8
pranav2008 <pranavdude2008@gmail.com>
2025-08-05 11:01:14 -0700

    added get functions for all user class attributes

--- Users.py
+++ Users.py
@@ -1,7 +1,23 @@
 class Users:
-    def __init__(self, username, password, id, age, sex):
+    def __init__(self, username, password, user_id, age, sex):
         self.username = username
         self.password = password
-        self.id = id
+        self.user_id = user_id
         self.age = age
         self.sex = sex
+
+    def get_username(self):
+        return self.username
+
+    def get_password(self):
+        return self.password
+
+    def get_id(self):
+        return self.user_id
+
+    def get_age(self):
+        return self.age
+
+    def get_sed(self):
+        return self.sex
+
