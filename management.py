8303f7683c66acee7a89a205a1d1af6612317b60
MaldBaller <mald.baller@gmail.com>
2025-08-05 15:15:23 -0700

    test2

--- management.py
+++ management.py
@@ -5,15 +5,17 @@ class Management:
     def __init__(self):
         self.users = []
         self.reports = []
-        self.repotsNumber = 0
-        self.usersNumber = 0
+        self.repots_number = 0
+        self.users_number = 0
 
     def getUserRepot(self):
         return self.reports
 
-    def addReport(self,report):m
+    def addReport(self,report):
         self.reports.append(report)
 
     def viewAllUserReport(self, id):
-        # for i in range(self.repotsNumber):
-        #     if id == self.reports[i].a
+        # for i in range(self.repots_number):
+        #     if id == self.reports[i].user_id:
+
+    #def getUser
