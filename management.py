66670f4236a58b570c82ea9e6c2e6b92a266c22f
MaldBaller <mald.baller@gmail.com>
2025-08-05 11:45:46 -0700

    updated mamangement class

--- management.py
+++ management.py
@@ -3,14 +3,17 @@ import numpy as np
 
 class Management:
     def __init__(self):
-        users = []
-        reports = []
-        repotsNumber = 0
-        usersNumber = 0
+        self.users = []
+        self.reports = []
+        self.repotsNumber = 0
+        self.usersNumber = 0
 
     def getUserRepot(self):
         return self.reports
 
-    def addReport(self,report):
+    def addReport(self,report):m
         self.reports.append(report)
 
+    def viewAllUserReport(self, id):
+        # for i in range(self.repotsNumber):
+        #     if id == self.reports[i].a
