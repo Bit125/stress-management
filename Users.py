03c189aa6c0e9583164445d0b6fdcf83e83596a7
pranav2008 <pranavdude2008@gmail.com>
2025-08-05 11:10:49 -0700

    added set functions for all user class attributes

--- Users.py
+++ Users.py
@@ -21,3 +21,18 @@ class Users:
     def get_sed(self):
         return self.sex
 
+    def set_username(self, new_username):
+        self.username = new_username
+
+    def set_password(self, new_password):
+        self.password = new_password
+
+    def set_user_id(self, new_user_id):
+        self.user_id = new_user_id
+
+    def set_age(self, new_age):
+        self.age = new_age
+
+    def set_sex(self, new_sex):
+        self.sex = new_sex
+
