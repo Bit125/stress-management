df0733cd63af9ab9d5e1694c2944b2a8c9f55c7e
pranav2008 <pranavdude2008@gmail.com>
2025-08-05 10:44:25 -0700

    added users class

---
+++ Users.py
@@ -0,0 +1,7 @@
+class Users:
+    def __init__(self, username, password, id, age, sex):
+        self.username = username
+        self.password = password
+        self.id = id
+        self.age = age
+        self.sex = sex
