01e506573980f673761aa38662f84d054678663b
Bit125 <ruleisaac44@gmail.com>
2025-08-05 11:47:44 -0700

    removed testing stuff

--- reports.py
+++ reports.py
@@ -7,10 +7,3 @@ class Report:
         self.user_id = user
         #the date AND time of the report
         self.date_and_time = datetime.datetime.now()
-
-report1 = Report(8, 0)
-print(report1.date_and_time)
-
-time.sleep(5)
-report2 = Report(2, 0)
-print(report2.date_and_time)
