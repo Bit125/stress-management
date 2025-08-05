83ec3987a06f07d2099f81820164f8e44a09fa1f
Bit125 <ruleisaac44@gmail.com>
2025-08-05 12:10:31 -0700

    added getters for attributes

--- reports.py
+++ reports.py
@@ -1,5 +1,5 @@
 import datetime
-import time
+#import time
 
 class Report:
     def __init__(self, level, user):
@@ -7,3 +7,15 @@ class Report:
         self.user_id = user
         #the date AND time of the report
         self.date_and_time = datetime.datetime.now()
+
+    def __str__(self):
+        return "User ID: " + str(self.user_id) + ", Time: " + str(self.date_and_time)
+
+    def get_stress_level(self):
+        return self.stress_level
+
+    def get_user_id(self):
+        return self.user_id
+
+    def get_date_and_time(self):
+        return self.date_and_time
