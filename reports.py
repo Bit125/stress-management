44b4f4f900d537e88f48dfd156abc952f2da1f6d
Bit125 <ruleisaac44@gmail.com>
2025-08-05 11:31:34 -0700

    commented a little bit

--- reports.py
+++ reports.py
@@ -5,6 +5,7 @@ class Report:
     def __init__(self, level, user):
         self.stress_level = level
         self.user_id = user
+        #the date AND time of the report
         self.date_and_time = datetime.datetime.now()
 
 report1 = Report(8, 0)
